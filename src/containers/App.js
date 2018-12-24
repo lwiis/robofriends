8cd16bff31a32ab9ae70275e11d700eb5dc9cf38
Luis Figueira <luis.filipe.figueira@gmail.com>
2018-12-24 22:46:52 +0000

    version with redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,51 +1,74 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 //import {robots} from './robots.js';
 import SearchBox from '../components/SearchBox.js';
 import './App.css';
 import Scroll from '../components/Scroll.js';
+import ErrorBoundary from '../components/ErrorBoundary.js';
+import { setSearchField, requestRobots } from '../actions';
+
+const mapStateToProps = (state) => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    }
+}
 
 class App extends Component {
+    /*
     constructor() {
         super();
         this.state = {
-            robots: [],
-            searchField: '',
+            robots: []
         }
     }
+    */
 
     componentDidMount() {
+        /*
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response=>response.json())
         .then(users=>this.setState({robots: users}));
         //his.setState({robots: robots});
-    }
-
-    onSearchChange = (event) => {
-        // warning!"this" refers to the object that raises the event
-        // when used in this method, unless the method is created 
-        // with arrow functions
-        this.setState({searchField: event.target.value});
+        */
+        this.props.onRequestRobots();
     }
 
     render() {
-        const {robots, searchField} = this.state;
+        //const {robots} = this.state;
+        const { searchField, onSearchChange, robots, isPending } = this.props;
         const filteredRobots = robots.filter(robot => robot.name.toLowerCase().includes(searchField.toLowerCase()));
-        
-        if(robots.length===0) {
-            return <h1 className='tc'>Loading...</h1>
-        }
-        
+
+        //if(robots.length===0) {
+        //if(isPending) {
+        //    return <h1 className='tc'>Loading...</h1>
+        //}
+        //else {
         return (
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
+                <SearchBox searchChange={onSearchChange} />
                 <Scroll>
-                    <CardList cards={filteredRobots}/>
+                    {isPending ? <h1>Loading</h1> :
+                        <ErrorBoundary>
+                            <CardList cards={filteredRobots} />
+                        </ErrorBoundary>
+                    }
                 </Scroll>
             </div>
         );
+        //}
     }
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+
+import { Provider } from 'react-redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
 import 'tachyons';
+
 import App from './containers/App';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+import { requestRobots, searchRobots } from './reducers'
+
+import * as serviceWorker from './serviceWorker';
+
+
+//const store = createStore(searchRobots);
+
+const logger = createLogger();
+
+const rootReducers = combineReducers({ searchRobots, requestRobots });
+
+// the following line didn't work when passing only the single reducer dfirectly
+// this ia because with a single reducer, state.REDUCER.field doesn't exist, it has to be 
+// state.field
+//const store = createStore(rootReducers, applyMiddleware(logger));
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
